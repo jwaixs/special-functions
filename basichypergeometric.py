3018a70f5ee9753ffb8de478d5fd4e7df2428d44
Noud Aldenhoven <noud.aldenhoven@gmail.com>
2013-01-11 13:18:18 +0100

    Doctests for BasicHypergeometricSeries.

--- basichypergeometric.py
+++ basichypergeometric.py
@@ -110,7 +110,7 @@ class qPochhammerSymbol():
         """
         EXAMPLES::
 
-            sage: q, a, b, n = var('q, a b n')
+            sage: q, a, b, n = var('q a b n')
             sage: qPochhammerSymbol(5, 1/2, 3)
             (5;1/2)_3
             sage: qPochhammerSymbol(a, q, 3)
@@ -133,9 +133,9 @@ class qPochhammerSymbol():
             sage: q, a, n = var('q a n')
             sage: qPochhammerSymbol(5, 1/2, 3).evaluate()
             -3/2
-            sage: qPochhammerSymbol(a, 3).evaluate()
+            sage: qPochhammerSymbol(a, q, 3).evaluate()
             -(a - 1)*(a*q - 1)*(a*q^2 - 1)
-            sage: qPochhammerSymbol(a, n).evaluate()
+            sage: qPochhammerSymbol(a, q, n).evaluate()
             0
         
         """
@@ -152,7 +152,7 @@ class qPochhammerSymbol():
 
         EXAMPLES::
 
-            sage: q, a, b, n = var('1 a b n')
+            sage: q, a, b, n = var('q a b n')
             sage: qPochhammerSymbol(5, 1/2, 3).evaluate()
             -3/2
             sage: qPochhammerSymbol(a, q, 3).evaluate()
@@ -187,61 +187,26 @@ class BasicHypergeometricSeries():
 
     EXAMPLES::
 
-        This example shows the basic properties of a summable Hypergeometric 
-        series::
-
-            sage: a, b, z = var('a b z')
-            sage: hs = HypergeometricSeries([a, -3], [b], z)
-            sage: hs
-            (2)_F_(1)(a,-3;b;z)
-            sage: latex(hs)
-            {}_{2}F_{1}\biggl[\genfrac..{0pt}{}{a,-3}{b};z\biggr]
-            sage: hs.evaluate()
-            -(a + 1)*(a + 2)*a*z^3/((b + 1)*(b + 2)*b) + 3*(a + 1)*a*z^2/((b + 1)*b) - 3*a*z/b + 1           
- 
-            sage: hs[3]
-            -(a + 1)*(a + 2)*a*z^3/((b + 1)*(b + 2)*b)
-            sage: hs[4]
-            0
-            sage: hs[1:3]
-            [-3*a*z/b, 3*(a + 1)*a*z^2/((b + 1)*b)]
-            sage: hs[0:]
-            [1, -3*a*z/b, 3*(a + 1)*a*z^2/((b + 1)*b), -(a + 1)*(a + 2)*a*z^3/((b + 1)*(b + 2)*b)]            
- 
-            sage: for coef in hs: print coef
-            1
-            -3*a*z/b
-            3*(a + 1)*a*z^2/((b + 1)*b)
-            -(a + 1)*(a + 2)*a*z^3/((b + 1)*(b + 2)*b)
-
-        We can for example approximate the exponential, sine and cosine using 
+        This example shows the basic properties of a summable Basic 
         Hypergeometric series::
 
-            sage: z = var('z')
-
-            sage: hs_exp = HypergeometricSeries([], [], z)
-            sage: sum(hs_exp[:5])
-            1/24*z^4 + 1/6*z^3 + 1/2*z^2 + z + 1
-            sage: exp(z).taylor(z, 0, 4)
-            1/24*z^4 + 1/6*z^3 + 1/2*z^2 + z + 1
-            sage: bool(sum(hs_exp[:5]) == exp(z).taylor(z, 0, 4))
-            True
-
-            sage: hs_cos = HypergeometricSeries([], [1/2], -1/4*z**2)
-            sage: sum(hs_cos[:5])
-            1/40320*z^8 - 1/720*z^6 + 1/24*z^4 - 1/2*z^2 + 1
-            sage: cos(z).taylor(z, 0, 8)
-            1/40320*z^8 - 1/720*z^6 + 1/24*z^4 - 1/2*z^2 + 1
-            sage: bool(cos(z).taylor(z, 0, 8) == sum(hs_cos[:5]))
-            True
-            
-            sage: hs_sin = HypergeometricSeries([], [3/2], -1/4*z**2)
-            sage: z*sum(hs_sin[:5])
-            1/362880*(z^8 - 72*z^6 + 3024*z^4 - 60480*z^2 + 362880)*z
-            sage: sin(z).taylor(z, 0, 9)
-            1/362880*z^9 - 1/5040*z^7 + 1/120*z^5 - 1/6*z^3 + z
-            sage: bool(sin(z).taylor(z, 0, 9) == z*sum(hs_sin[:5]))
-            True
+            sage: a, b, q, z = var('a b q z')
+            sage: bhs = BasicHypergeometricSeries([a, q**(-3)], [b], q, z)
+            sage: bhs
+            (2)_phi_(1)(a,q^(-3);b;q,z)
+            sage: bhs.evaluate()
+            -(1/q^3 - 1)*(1/q^2 - 1)*(1/q - 1)*(a - 1)*(a*q - 1)*(a*q^2 - 1)*q^3*z^3/((q - 1)*(b - 1)*(q^2 - 1)*(b*q - 1)*(q^3 - 1)*(b*q^2 - 1)) + (1/q^3 - 1)*(1/q^2 - 1)*(a - 1)*(a*q - 1)*q*z^2/((q - 1)*(b - 1)*(q^2 - 1)*(b*q - 1)) - (1/q^3 - 1)*(a - 1)*z/((q - 1)*(b - 1)) + 1
+            sage: bhs[3]
+            -(1/q^3 - 1)*(1/q^2 - 1)*(1/q - 1)*(a - 1)*(a*q - 1)*(a*q^2 - 1)*q^3*z^3/((q - 1)*(b - 1)*(q^2 - 1)*(b*q - 1)*(q^3 - 1)*(b*q^2 - 1))
+            sage: bhs[4]
+            0
+            sage: bhs[1:3]
+            [-(1/q^3 - 1)*(a - 1)*z/((q - 1)*(b - 1)), (1/q^3 - 1)*(1/q^2 - 1)*(a - 1)*(a*q - 1)*q*z^2/((q - 1)*(b - 1)*(q^2 - 1)*(b*q - 1))]                                         
+            sage: for coef in bhs: print coef
+            1
+            -(1/q^3 - 1)*(a - 1)*z/((q - 1)*(b - 1))
+            (1/q^3 - 1)*(1/q^2 - 1)*(a - 1)*(a*q - 1)*q*z^2/((q - 1)*(b - 1)*(q^2 - 1)*(b*q - 1))
+            -(1/q^3 - 1)*(1/q^2 - 1)*(1/q - 1)*(a - 1)*(a*q - 1)*(a*q^2 - 1)*q^3*z^3/((q - 1)*(b - 1)*(q^2 - 1)*(b*q - 1)*(q^3 - 1)*(b*q^2 - 1))
 
     """ 
     def __init__(self, list_a, list_b, q, z):
@@ -250,9 +215,9 @@ class BasicHypergeometricSeries():
 
         EXAMPLES::
 
-            sage: a, b, c, z = var('a b c z')
-            sage: HypergeometricSeries([a, b], [c], z)
-            (2)_F_(1)(a,b;c;z)
+            sage: a, b, c, q, z = var('a b c q z')
+            sage: BasicHypergeometricSeries([a, b], [c], q, z)
+            (2)_phi_(1)(a,b;c;q,z)
 
         """
         self.list_a = list_a
@@ -264,9 +229,9 @@ class BasicHypergeometricSeries():
         """
         EXAMPLES::
 
-            sage: a, b, c, z = var('a b c z')
-            sage: latex(HypergeometricSeries([a, b], [c], z))
-            {}_{2}F_{1}\biggl[\genfrac..{0pt}{}{a,b}{c};z\biggr]
+            sage: a, b, c, q, z = var('a b c q z')
+            sage: latex(BasicHypergeometricSeries([a, b], [c], q, z))
+            {}_{2}\phi_{1}\biggl[\genfrac..{0pt}{}{a,b}{c};q,z\biggr]
 
         """
         return "{}_{%i}\phi_{%i}\\biggl[\genfrac..{0pt}{}{%s}{%s};%s,%s\\biggr]" \
@@ -279,9 +244,9 @@ class BasicHypergeometricSeries():
         """
         EXAMPLES::
 
-            sage: a, b, c, z = var('a b c z')
-            sage: HypergeometricSeries([a, b], [c], z)
-            (2)_F_(1)(a,b;c;z)
+            sage: a, b, c, q, z = var('a b c q z')
+            sage: BasicHypergeometricSeries([a, b], [c], q, z)
+            (2)_phi_(1)(a,b;c;q,z)
 
         """
         return "(%i)_phi_(%i)(%s;%s;%s,%s)" % (len(self.list_a), \
@@ -294,14 +259,14 @@ class BasicHypergeometricSeries():
         """
         EXAMPLES::
 
-            sage: a, b, c, z = var('a b c z')
-            sage: hs = HypergeometricSeries([a, b], [c], z)
-            sage: for i in range(4): print hs[i]
+            sage: a, b, c, q, z = var('a b c q z')
+            sage: bhs = BasicHypergeometricSeries([a, b], [c], q, z)
+            sage: for i in range(4): print bhs[i]
             1
-            a*b*z/c
-            1/2*(b + 1)*(a + 1)*a*b*z^2/((c + 1)*c)
-            1/6*(b + 1)*(b + 2)*(a + 1)*(a + 2)*a*b*z^3/((c + 1)*(c + 2)*c)
-
+            -(b - 1)*(a - 1)*z/((q - 1)*(c - 1))
+            (b - 1)*(a - 1)*(b*q - 1)*(a*q - 1)*q*z^2/((q - 1)*(c - 1)*(q^2 - 1)*(c*q - 1))
+            -(b - 1)*(a - 1)*(b*q - 1)*(a*q - 1)*(b*q^2 - 1)*(a*q^2 - 1)*q^3*z^3/((q - 1)*(c - 1)*(q^2 - 1)*(c*q - 1)*(q^3 - 1)*(c*q^2 - 1))
+        
         """
         if key >= 0:
             nominator = qPochhammerSymbol(self.list_a, self.q, key).evaluate()
@@ -320,15 +285,15 @@ class BasicHypergeometricSeries():
         """
         EXAMPLES::
 
-            sage: a, b, c, z = var('a b c z')
-            sage: hs = HypergeometricSeries([a, b], [c], z)
-            sage: hs[0:3]
-            [1, a*b*z/c, 1/2*(b + 1)*(a + 1)*a*b*z^2/((c + 1)*c)]
-            sage: hs[1:3]
-            [a*b*z/c, 1/2*(b + 1)*(a + 1)*a*b*z^2/((c + 1)*c)]
-            sage: hs[:2]
-            [1, a*b*z/c]
-            sage: hs[3:2]
+            sage: a, b, c, q, z = var('a b c q z')
+            sage: bhs = BasicHypergeometricSeries([a, b], [c], q, z)
+            sage: bhs[0:3]
+            [1, -(b - 1)*(a - 1)*z/((q - 1)*(c - 1)), (b - 1)*(a - 1)*(b*q - 1)*(a*q - 1)*q*z^2/((q - 1)*(c - 1)*(q^2 - 1)*(c*q - 1))]
+            sage: bhs[1:3]
+            [-(b - 1)*(a - 1)*z/((q - 1)*(c - 1)), (b - 1)*(a - 1)*(b*q - 1)*(a*q - 1)*q*z^2/((q - 1)*(c - 1)*(q^2 - 1)*(c*q - 1))]
+            sage: bhs[:2]
+            [1, -(b - 1)*(a - 1)*z/((q - 1)*(c - 1))]
+            sage: bhs[3:2]
             []
 
         """
@@ -348,14 +313,13 @@ class BasicHypergeometricSeries():
         """
         EXAMPLES::
 
-            sage: a, b, z = var('a b z')
-            sage: hs = HypergeometricSeries([a, -3], [b], z)
-            sage: for coef in hs: print coef
+            sage: a, b, q, z = var('a b q z')
+            sage: bhs = BasicHypergeometricSeries([a, q**(-3)], [b], q, z)
+            sage: for coef in bhs: print coef
             1
-            -3*a*z/b
-            3*(a + 1)*a*z^2/((b + 1)*b)
-            -(a + 1)*(a + 2)*a*z^3/((b + 1)*(b + 2)*b)
-
+            -(1/q^3 - 1)*(a - 1)*z/((q - 1)*(b - 1))
+            (1/q^3 - 1)*(1/q^2 - 1)*(a - 1)*(a*q - 1)*q*z^2/((q - 1)*(b - 1)*(q^2 - 1)*(b*q - 1))
+            -(1/q^3 - 1)*(1/q^2 - 1)*(1/q - 1)*(a - 1)*(a*q - 1)*(a*q^2 - 1)*q^3*z^3/((q - 1)*(b - 1)*(q^2 - 1)*(b*q - 1)*(q^3 - 1)*(b*q^2 - 1))
         """
         i = 0
         coef = self[i]
@@ -369,10 +333,10 @@ class BasicHypergeometricSeries():
         """
         EXAMPLES::
 
-            sage: a, b, z = var('a b z')
-            sage: hs = HypergeometricSeries([a, -3], [b], z)
-            sage: hs.evaluate()
-            -(a + 1)*(a + 2)*a*z^3/((b + 1)*(b + 2)*b) + 3*(a + 1)*a*z^2/((b + 1)*b) - 3*a*z/b + 1
+            sage: a, b, q, z = var('a b q z')
+            sage: bhs = BasicHypergeometricSeries([a, q**(-3)], [b], q, z)
+            sage: bhs.evaluate()
+            -(1/q^3 - 1)*(1/q^2 - 1)*(1/q - 1)*(a - 1)*(a*q - 1)*(a*q^2 - 1)*q^3*z^3/((q - 1)*(b - 1)*(q^2 - 1)*(b*q - 1)*(q^3 - 1)*(b*q^2 - 1)) + (1/q^3 - 1)*(1/q^2 - 1)*(a - 1)*(a*q - 1)*q*z^2/((q - 1)*(b - 1)*(q^2 - 1)*(b*q - 1)) - (1/q^3 - 1)*(a - 1)*z/((q - 1)*(b - 1)) + 1
 
         """
         return sum(list(self))
