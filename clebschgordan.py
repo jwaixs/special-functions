62b070fda8ee227787d216d87f4891159d15fa2c
Noud Aldenhoven <noud.aldenhoven@gmail.com>
2013-01-17 10:23:28 +0100

    Change of polynomials.

--- clebschgordan.py
+++ clebschgordan.py
@@ -1,7 +1,7 @@
-from basichypergeometric import qPochhammerSymbol, BasicHypergeometricSeries
+from basichypergeometric import qPochhammerSymbol
 from qpolynomials import dual_q_Hahn_polynomials
 
-def coef(l1, l2, l, p, n, algorithm='mathematica'):
+def coef(l1, l2, l, p, n, algorithm=None):
     q = var('q')
 
     if p >= 0:
@@ -11,19 +11,19 @@ def coef(l1, l2, l, p, n, algorithm='mathematica'):
         qpoch2 = qPochhammerSymbol([q**2, q**(2*p + 2)], q**2, n)
         result2 = sqrt(qpoch1.evaluate() / qpoch2.evaluate())
         
-        list1 = [q**(-2*n), q**(-2*l1 - 2*l2 + 2*l), q**(-2*l1 - 2*l2 - 2*l - 2)]
-        list2 = [q**(-4*l1 + 2*p), q**(-4*l2)]
-        result3 = BasicHypergeometricSeries(list1, list2, q**2, q**2).evaluate()
+        #list1 = [q**(-2*n), q**(-2*l1 - 2*l2 + 2*l), q**(-2*l1 - 2*l2 - 2*l - 2)]
+        #list2 = [q**(-4*l1 + 2*p), q**(-4*l2)]
+        #result3 = BasicHypergeometricSeries(list1, list2, q**2, q**2).evaluate()
         
-        if p <= 2*l1 - 2*l2 and n == 2*l2:
+        if p <= 2*l1 - 2*l2:
             result3 = dual_q_Hahn_polynomials( \
                 n, l1 + l2 - l, q**(-4*l1 + 2*p - 2), \
                 q**(-4*l2 - 2*p - 2), 2*l2, q**2 \
             )
-        elif p >= 2*l1 - 2*l2 and n == 2*l1 - 2*p:
+        else:
             result3 = dual_q_Hahn_polynomials( \
-                n, l1 + l2 - n, q**(-4*l1 + 2*p - 2), \
-                q**(-4*l2 - 2*p - 2), 2**l1 - 2*p, q**2
+                n, l1 + l2 - n, q**(-4*l2 - 2), \
+                q**(-4*l1 - 2), 2**l1 - 2*p, q**2
             )
 
     else:
